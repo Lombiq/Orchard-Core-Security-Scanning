5fc4c7a221786a679767229de8a4b3920aabf868
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-09-04 23:46:01 +0200

    Simplifying sample code

--- test/SecurityScanning.Web.Tests.UI/Tests/SecurityScanningTests.cs
+++ test/SecurityScanning.Web.Tests.UI/Tests/SecurityScanningTests.cs
@@ -3,7 +3,6 @@ using Shouldly;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
-using YamlDotNet.RepresentationModel;
 
 namespace SecurityScanning.Web.Tests.UI.Tests;
 
@@ -28,34 +27,8 @@ public class SecurityScanningTests : UITestBase
                     .ExcludeUrlWithRegex(".*blog.*")
                     .DisablePassiveScanRule(10020, "The response does not include either Content-Security-Policy with 'frame-ancestors' directive.")
                     .DisableScanRuleForUrlWithRegex(".*/about", 10038, "Content Security Policy (CSP) Header Not Set")
-                    .ModifyZapPlan(plan => plan
-                        .AddFalsePositiveRuleFilter(
-                            ".*",
-                            10202,
-                            "Absence of Anti-CSRF Tokens",
-                            "The search form doesn't alter the state of the application so anti-CSRF tokens are not needed.",
-                            node =>
-                            {
-                                node.Children["evidence"] = ".*my-search-form.*";
-                                node.Children["evidenceRegex"] = "true";
-                            }))
                     .SignIn(),
                 sarifLog => sarifLog.Runs[0].Results.Count.ShouldBe(0)),
-            changeConfiguration: configuration => configuration.UseAssertAppLogsForSecurityScan());
-
-    [Fact]
-    public Task SecurityScanWithCustomAutomationFrameworkPlanShouldPass() =>
-        ExecuteTestAfterBrowserSetupWithoutBrowserAsync(
-            context => context.RunAndAssertSecurityScanAsync(
-                "Tests/CustomZapAutomationFrameworkPlan.yml",
-                configuration => configuration
-                    .ModifyZapPlan(plan =>
-                    {
-                        var spiderJob = plan.GetSpiderJob();
-                        var spiderParameters = (YamlMappingNode)spiderJob["parameters"];
-                        spiderParameters.Add("maxDepth", "8");
-                    }),
-                sarifLog => SecurityScanningConfiguration.AssertSecurityScanHasNoAlerts(context, sarifLog)),
             changeConfiguration: configuration => configuration.UseAssertAppLogsForSecurityScan());
 
     [Fact]
